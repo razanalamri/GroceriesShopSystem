c711e297e289973e36798e71e63dc9c29b176484
razanalamri <razan.f96@outlook.com>
2023-01-30 09:41:31 +0400

    Adding Header table with rebort Function

--- reportOfInvoice.java
+++ reportOfInvoice.java
@@ -68,9 +68,16 @@ public class reportOfInvoice {
 			         System.out.println("Please Enter the number of rows :");
 			         int userinput=scanner1.nextInt();
 			         int count=0;
-			         String sql="SELECT * FROM Customer";
+			         String sql="SELECT * FROM Customer JOIN Item ON Item.id = Customer.Item_id JOIN Shop ON Shop.id = Item.Shop_id JOIN Header ON Header.id = Shop.Header_id";
 				     ResultSet result=st.executeQuery(sql);
 				     while(result.next()&&count<userinput) {
+				    	 
+				    	 
+				    	 
+				    	 
+				    	 
+				    	 
+				    	 
 				    	
 				    	Integer Invoice_No=result.getInt("Invoice_No");
 				    	String Invoice_date=result.getString("Invoice_date");
@@ -78,10 +85,19 @@ public class reportOfInvoice {
 				    	 Integer Number_Of_Items=result.getInt("Number_Of_Items");
 				    	 float Total_Amount=result.getFloat("Total_Amount");
 				    	 float Balance=result.getFloat("Balance");
-				    	 
-				    	
-				    
-		   	  System.out.println(Invoice_No+" "+Invoice_date+" "+Customer_Name+" "+Number_Of_Items+" "+Total_Amount+" "+Balance);
+				    	 String Item_Name=result.getString("Item_Name");
+				    	 float Unit_price=result.getFloat("Unit_price");
+				    	 Integer Quntity=result.getInt("Quntity");
+				    	 float Qty=result.getFloat("Qty");
+				    	 String Shop_Name=result.getString("Shop_Name");
+				    	 String Tel=result.getString("Tel");
+				    	 String Fax=result.getString("Fax");
+				    	 String Email=result.getString("Email");
+				    	 String Website=result.getString("Website");
+			
+				    	 System.out.println("Shop Name :"+Shop_Name);
+				    	 System.out.println("Telephone :"+Tel+"------Fax :"+Fax+"-------Website :"+Website);
+		   	  System.out.println(Invoice_No+" "+Invoice_date+" "+Customer_Name+" "+Number_Of_Items+" "+Total_Amount+" "+Balance+" "+Item_Name+" "+Unit_price+" "+Quntity+" "+Qty);
 		   	  count++;
 		      
 		      
