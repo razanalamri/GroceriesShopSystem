7348c2d298f861c68b3a5a2b3b6bfcf4be057249
razanalamri <razan.f96@outlook.com>
2023-02-02 09:17:43 +0400

    Adding setter and getter on customer class

--- Customer.java
+++ Customer.java
@@ -1,5 +1,59 @@
 
 public class Customer {
+	public Item[] getItem() {
+		return Item;
+	}
+	public void setItem(Item[] item) {
+		Item = item;
+	}
+	public int getInvoice_No() {
+		return Invoice_No;
+	}
+	public void setInvoice_No(int invoice_No) {
+		Invoice_No = invoice_No;
+	}
+	public String getInvoice_date() {
+		return Invoice_date;
+	}
+	public void setInvoice_date(String invoice_date) {
+		Invoice_date = invoice_date;
+	}
+	public String getCustomer_Name() {
+		return Customer_Name;
+	}
+	public void setCustomer_Name(String customer_Name) {
+		Customer_Name = customer_Name;
+	}
+	public String getPhone_Number() {
+		return Phone_Number;
+	}
+	public void setPhone_Number(String phone_Number) {
+		Phone_Number = phone_Number;
+	}
+	public int getNumber_Of_Items() {
+		return Number_Of_Items;
+	}
+	public void setNumber_Of_Items(int number_Of_Items) {
+		Number_Of_Items = number_Of_Items;
+	}
+	public float getTotal_Amount() {
+		return Total_Amount;
+	}
+	public void setTotal_Amount(float total_Amount) {
+		Total_Amount = total_Amount;
+	}
+	public float getPaid_Amount() {
+		return Paid_Amount;
+	}
+	public void setPaid_Amount(float paid_Amount) {
+		Paid_Amount = paid_Amount;
+	}
+	public float getBalance() {
+		return Balance;
+	}
+	public void setBalance(float balance) {
+		Balance = balance;
+	}
 	Item[]Item;
 	int Invoice_No;
 	String Invoice_date;
